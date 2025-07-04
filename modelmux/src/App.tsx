7572aa24f3b65204cafe89a58a73a38230d925b3
SlothfulDreams <bensonzheng87901@gmail.com>
2025-07-04 15:34:49 -0400

    tailwind set up

--- modelmux/src/App.tsx
+++ modelmux/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div>
+    <div className="bg-red-100 min-h-screen items-center justify-center">
       <h1>ModelMux Application</h1>
       <p>This is a placeholder for the ModelMux application.</p>
     </div>
--- modelmux/src/renderer.tsx
+++ modelmux/src/renderer.tsx
@@ -26,13 +26,13 @@
  * ```
  */
 
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';    
-import App from './App';
+import "./global.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
