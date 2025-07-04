9741b78cb91edccaee3b21208783e9b77782fd2b
SlothfulDreams <bensonzheng87901@gmail.com>
2025-07-04 14:48:01 -0400

    inital

---
+++ modelmux/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>ModelMux Application</h1>
+      <p>This is a placeholder for the ModelMux application.</p>
+    </div>
+  );
+}
+
+export default App;
