74b7e8616cf33e2a2068dac69227ad449cfea829
SlothfulDreams <bensonzheng87901@gmail.com>
2025-07-17 22:45:08 -0400

    UI component for manage models

--- modelmux/src/App.tsx
+++ modelmux/src/App.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { AppSidebar } from "./components/ChatInterface/app-sidebar";
 import { SidebarProvider, SidebarInset } from "./components/ui/sidebar";
 import { ChatInterface } from "@/components/ChatInterface/chat-interface";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ManageModel } from "@/components/manage-model";
+
+export type View = "chat" | "models";
 
 function App() {
+  const [currentView, setCurrentView] = useState<View>("chat");
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <SidebarProvider>
-        <AppSidebar />
+        <AppSidebar setCurrentView={setCurrentView} />
         <SidebarInset>
-          <ChatInterface />
+          {currentView === "chat" && <ChatInterface />}
+          {currentView === "models" && <ManageModel />}
         </SidebarInset>
       </SidebarProvider>
     </ThemeProvider>
