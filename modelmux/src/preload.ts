b6e701828dd66bb44f123921ce2a9d5aff8bd808
SlothfulDreams <bensonzheng87901@gmail.com>
2025-07-31 21:31:14 -0400

    context brige

--- modelmux/src/preload.ts
+++ modelmux/src/preload.ts
@@ -1,2 +1,11 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from "electron";
+import { search } from "./lib/duckduckgo";
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  searchDuckDuckGo: async (url: string) => {
+    const { apiKey, profile } = await ipcRenderer.invoke("get-credentials");
+    return search(url, apiKey, profile);
+  },
+});
